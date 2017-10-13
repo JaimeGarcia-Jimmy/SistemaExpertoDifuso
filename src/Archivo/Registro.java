0b6770b22349e10084c6a21f3ec2856065b16b52
JohnMikeKS <johnmikelp@gmail.com>
2017-10-13 17:30:36 -0500

    Clase Registro
    Esta clase es el "Adaptador" del archivo Binario genera un ArrayList<Variable>

--- src/Archivo/Registro.java
+++ src/Archivo/Registro.java
@@ -6,101 +6,124 @@
 package Archivo;
 import Logica.Valor;
 import Logica.Punto;
+import Logica.Variable;
 
 import java.io.*; 
 import java.text.ParseException;
 import java.util.ArrayList;
 public class Registro extends Files { 
-  public static int DIM = 64; 
-  String varLinguistica;
-  Valor F1;
-    public String getVarLinguistica() {
-        return varLinguistica;
-    }
-    public void setVarLinguistica(String varLinguistica) {
-        this.varLinguistica = varLinguistica;
-    }
-    public Valor getF1() {
-        return F1;
-    }
-    public void setF1(Valor F1) {
-        this.F1 = F1;
-    }
     
+  public static int DIM = 1108; 
+  ArrayList<Variable> variables;
+  
+    public ArrayList<Variable> getVariable() {
+        return variables;
+    }
+
+    public void setVariable(ArrayList<Variable> variable) {
+        this.variables = variable;
+    }
   // Lectura 
-public void read (RandomAccessFile file) throws IOException, ParseException 
+  public void read (RandomAccessFile file) throws IOException, ParseException 
   { 
-    setVarLinguistica(readString(file , 20));
-    setF1( readFuncionM (file));
+    setVariable(readArrayVariable(file));
   } 
-  private String readString(RandomAccessFile file, int dim)throws IOException 
+  private String readString(RandomAccessFile file, int dim) throws IOException 
   { 
     char campo[] = new char[dim]; 
     for (int i=0; i<dim; i++) 
         campo[i] = file.readChar(); 
     return new String(campo).replace('\0',' '); 
   } 
-private ArrayList<Punto> readArray(RandomAccessFile file, int dim) throws IOException
-{
-    ArrayList<Punto> puntos = new ArrayList();
-    puntos.add(new Punto(file.readDouble(),file.readDouble()));
-    puntos.add(new Punto(file.readDouble(),file.readDouble()));
-    puntos.add(new Punto(file.readDouble(),file.readDouble()));
-    puntos.add(new Punto(file.readDouble(),file.readDouble()));
-    
-    return puntos;
-    
-}
-private Valor readFuncionM(RandomAccessFile file)throws IOException 
-{
-    Valor funcion = new Valor();
-    funcion.setNombre(readString( file , 20 ));
-    funcion.setPuntos(readArray ( file , 24 ));//8 por cada Double
-    return funcion;
-}
-  // Escritura 
-public void write (RandomAccessFile file)throws IOException 
-{ 
-    writeString     ( file, getVarLinguistica(), 20);
-    writeFuncionM   ( file, getF1(), 20);
-    file.writeDouble( getF1().getPuntos().get(0).getX() );
-    file.writeDouble( getF1().getPuntos().get(0).getY() );
-    file.writeDouble( getF1().getPuntos().get(1).getX() );
-    file.writeDouble( getF1().getPuntos().get(1).getY() );
-    file.writeDouble( getF1().getPuntos().get(2).getX() );
-    file.writeDouble( getF1().getPuntos().get(2).getY() );
-    file.writeDouble( getF1().getPuntos().get(3).getX() );
-    file.writeDouble( getF1().getPuntos().get(3).getY() );
-    
-} 
-
-  private void writeFuncionM(RandomAccessFile file, Valor funcion, int dim) throws IOException
+  private ArrayList<Variable> readArrayVariable(RandomAccessFile file) throws IOException
   {
-    StringBuffer buffer; 
-    if (funcion!=null)
-    {
+      
+      ArrayList<Variable> array = new ArrayList<Variable>();
+      file.seek(0);
+      Long numR = file.length()/DIM;
+      
+      for(int i=0;i<numR;i++)
+      {
+        Variable variable= new Variable();
+        variable.setNombre( readString(file,20));
+        variable.setAlias(  readString(file,10));
+        variable.setTraslape(file.readDouble());
         
-        buffer = new StringBuffer();
-        buffer.append(funcion.getNombre());//20
-        buffer.setLength(20);
-        file.writeChars(buffer.toString()); 
+        ArrayList<Valor> vl = new ArrayList();
         
-        
-    }
-    else
-    {
-        buffer = new StringBuffer();
+        for (int j = 0; j < 10; j++) 
+        {
+            Valor valor = new Valor();
+            valor.setNombre( readString(file,20));
+            ArrayList<Punto> punto = new ArrayList();
+            for (int k = 0; k < 4; k++) 
+            {
+                Punto puntos = new Punto( file.readDouble(), file.readDouble());
+                punto.add(puntos);
+            }
+            valor.setPuntos(punto);
+            vl.add(valor);
+            
+        }
+        variable.setFunciones(vl);
+        array.add(variable);
+      }
+      
+      return array;
+  }
+    // Escritura 
+    private void writeString (RandomAccessFile file, String str, int dim) throws IOException 
+    { 
+        StringBuffer buffer = new StringBuffer(); 
+        if (str!=null) 
+           buffer.append(str); 
         buffer.setLength(dim); 
         file.writeChars(buffer.toString()); 
     }
-  }
-  private void writeString(RandomAccessFile file, String str, int dim)throws IOException 
-  { 
-    StringBuffer buffer = new StringBuffer(); 
-    if (str!=null) 
-       buffer.append(str); 
-    
-    buffer.setLength(dim); 
-    file.writeChars(buffer.toString()); 
-  }
-} 
+    public void write (RandomAccessFile file, ArrayList<Variable> array)throws IOException 
+    {     
+        file.setLength(0);
+        file.seek(0);
+      
+        for(Variable v: array)
+        {
+            writeString(file,v.getNombre(),20);
+            writeString(file,v.getAlias(),10);
+            file.writeDouble(v.getTraslape());
+            ArrayList<Valor> vl = v.getFunciones();
+            if(vl.isEmpty())
+                writeString(file,"",520);
+            else
+            {
+                int i=0;
+                for(Valor val: vl)
+                {
+                    i++;
+                    writeString(file,val.getNombre(),20);
+                    ArrayList<Punto> pu= val.getPuntos();
+                    if(pu.isEmpty())
+                        writeString(file,"",64);
+                    else
+                    {
+                        int ii=0;
+                        for(Punto pun: pu)
+                        {
+                            ii++;
+                            //Escribir X
+                            file.writeDouble(pun.getX());
+
+                            //Escribir Y
+                            file.writeDouble(pun.getY());
+
+                        }
+                        for (int j=ii ; j<4 ; j++) 
+                            writeString(file,"",8);
+                    }
+                }
+                for(int j=i; j<10;j++)
+                    writeString(file,"",52);
+                
+            }
+        }
+}
+}
